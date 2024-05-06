5d0dee9143a33d913ffc9860d9d0bc0995e931f1
Sirmanu94 <emanueleavitabile1@gmail.com>
2024-05-06 14:18:07 +0200

    es2

---
+++ Es2/Es2.js
@@ -0,0 +1,10 @@
+$ node
+
+> const crypto = require('crypto');
+
+// Per visualizzare i metodi
+> crypto. //(se non funziona utilizzare console.log(Object.keys(crypto));)
+
+
+> const randomID = crypto.randomBytes(8).toString('hex'); //Per generar un id Random
+> randomID
